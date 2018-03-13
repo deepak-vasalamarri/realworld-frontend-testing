b70b3e4aa5ad375556a9e6095c9d527cdadf7378
Gil Tayar <gil@tayar.org>
2018-03-13 10:01:40 +0200

    added utility functions to simplify tests

--- test/functional.test.js
+++ test/functional.test.js
@@ -6,42 +6,48 @@ require('chromedriver')
 const {By, until, Key} = webdriver
 
 describe('registration page', function() {
-  const chromeCapabilities = webdriver.Capabilities.chrome()
-  chromeCapabilities.set('chromeOptions', {args: ['--headless']})
-
   let driver
-  before(
-    async () =>
-      (driver = await new webdriver.Builder()
-        .forBrowser('chrome')
-        .withCapabilities(chromeCapabilities)
-        .build()),
-  )
+  before(async () => (driver = await new webdriver.Builder().forBrowser('chrome').build()))
   after(async () => await driver.quit())
+
+  async function waitFor(selector) {
+    await driver.wait(until.elementLocated(By.css(selector)))
+  }
+
+  async function setText(selector, text) {
+    const field = await driver.findElement(By.css(selector))
+
+    await field.sendKeys(text)
+  }
+
+  async function click(selector) {
+    const element = await driver.findElement(By.css(selector))
+    await element.click()
+  }
+
+  async function getText(selector) {
+    const element = await driver.findElement(By.css(selector))
+
+    return await element.getText()
+  }
 
   it('should not allow a blank email', async () => {
     // action: browse to registration page
     await driver.get('http://localhost:3000/register')
 
-    await driver.wait(until.elementLocated(By.css('input[placeholder=Username]')))
+    await waitFor('input[placeholder=Username]')
 
     // action: set username and password to something, but leave email blank
-    const userNameField = await driver.findElement(By.css('input[placeholder=Username]'))
-    await userNameField.sendKeys('aUsername')
-
-    const passwordField = await driver.findElement(By.css('input[placeholder=Password]'))
-    await passwordField.sendKeys('aPassword')
+    await setText('input[placeholder=Username]', 'aUsername')
+    await setText('input[placeholder=Password]', 'aPassword')
 
     // action: submit form
-    const submit = await driver.findElement(By.css('button[type=submit]'))
-    await submit.click()
+    await click('button[type=submit]')
 
     // validate: there is an error message
-    await driver.wait(until.elementLocated(By.css('.error-messages')))
+    await waitFor('.error-messages')
 
-    const errorMessages = await driver.findElement(By.css('.error-messages'))
-
-    expect(await errorMessages.getText()).to.include("email can't be blank")
+    expect(await getText('.error-messages')).to.include("email can't be blank")
   })
 
   it('should do the main flow correctly', async () => {
@@ -60,34 +66,27 @@ describe('registration page', function() {
     await driver.get('http://localhost:3000/register')
 
     // action: set username, password, email to something
-    const userNameField = await driver.findElement(By.css('input[placeholder=Username]'))
-    await userNameField.sendKeys('ausername')
+    await setText('input[placeholder=Username]', 'ausername')
 
-    const passwordField = await driver.findElement(By.css('input[placeholder=Password]'))
-    await passwordField.sendKeys('aPassword')
+    await setText('input[placeholder=Password]', 'aPassword')
 
-    const emailField = await driver.findElement(By.css('input[placeholder=Email]'))
-    await emailField.sendKeys('an@email.com')
+    await setText('input[placeholder=Email]', 'an@email.com')
 
     // action: submit form
-    const submit = await driver.findElement(By.css('button[type=submit]'))
-    await submit.click()
+    await click('button[type=submit]')
   }
 
   async function validateUserHomePage() {
-    await driver.wait(until.elementLocated(By.css('img[alt=ausername]')))
+    await waitFor('img[alt=ausername]')
 
     // validate username
-    const usernameField = await driver.findElement(By.css('a[href="/@ausername"]'))
-    expect(await usernameField.getText()).to.equal('ausername')
+    expect(await getText('a[href="/@ausername"]')).to.equal('ausername')
 
     // validate articles list
-    const articlesList = await driver.findElement(By.css('.article-preview'))
-    expect(await articlesList.getText()).to.equal('No articles are here... yet.')
+    expect(await getText('.article-preview')).to.equal('No articles are here... yet.')
 
     // validate active tab
-    const yourFeedTab = await driver.findElement(By.css('.nav-link.active'))
-    expect(await yourFeedTab.getText()).to.equal('Your Feed')
+    expect(await getText('.nav-link.active')).to.equal('Your Feed')
   }
 
   async function publishPost() {
@@ -95,19 +94,12 @@ describe('registration page', function() {
     const newPost = await driver.findElement(By.partialLinkText('New Post'))
     await newPost.click()
 
-    await driver.wait(until.elementLocated(By.css('input[placeholder="Article Title"]')))
+    await waitFor('input[placeholder="Article Title"]')
 
     // action: set the title, description, and article
-    const title = await driver.findElement(By.css('input[placeholder="Article Title"]'))
-    await title.sendKeys('a title')
-
-    const description = await driver.findElement(
-      By.css('input[placeholder="What\'s this article about?"]'),
-    )
-    await description.sendKeys('something')
-
-    const article = await driver.findElement(By.css('textarea[placeholder*="Write your article"]'))
-    await article.sendKeys('wonderful')
+    await setText('input[placeholder="Article Title"]', 'a title')
+    await setText('input[placeholder="What\'s this article about?"]', 'something')
+    await setText('textarea[placeholder*="Write your article"]', 'wonderful')
 
     // action: set the tags
     const tags = await driver.findElement(By.css('input[placeholder="Enter tags"]'))
@@ -116,57 +108,48 @@ describe('registration page', function() {
     }
 
     // action: submit form
-    const newPostButton = await driver.findElement(By.css('button[type=button]'))
-    await newPostButton.click()
+    await click('button[type=button]')
   }
 
   async function validatePost() {
-    await driver.wait(until.elementLocated(By.css('h1')))
+    await waitFor('h1')
 
     // validate title
-    const title = await driver.findElement(By.css('h1'))
-    expect(await title.getText()).to.equal('a title')
+    expect(await getText('h1')).to.equal('a title')
 
     // validate article content
-    const article = await driver.findElement(By.css('div.article-content'))
-    expect(await article.getText()).to.include('wonderful')
+    expect(await getText('.article-content')).to.include('wonderful')
 
     // validate tags
-    const tags = await driver.findElement(By.css('ul.tag-list'))
-    expect(await tags.getText()).to.equal('abc')
+    expect(await getText('.tag-list')).to.equal('abc')
   }
 
   async function addComment() {
     // action: set comment
-    const commentInput = await driver.findElement(By.css('textarea'))
-    await commentInput.sendKeys('a comment')
+    await setText('textarea', 'a comment')
 
     // action: submit comment
-    const submitButton = await driver.findElement(By.css('button[type=submit]'))
-    await submitButton.click()
+    await click('button[type=submit]')
   }
 
   async function validateComment() {
-    await driver.wait(until.elementLocated(By.css('div.card .card-block')))
+    await waitFor('div.card .card-block')
 
     // validate comment text
-    const comment = await driver.findElement(By.css('div.card .card-block'))
-    expect(await comment.getText()).to.equal('a comment')
+    expect(await getText('div.card .card-block')).to.equal('a comment')
 
     // validate comment username
-    const username = await driver.findElement(By.css('div.card a.comment-author:nth-child(2)'))
-    expect(await username.getText()).to.equal('ausername')
+    expect(await getText('div.card a.comment-author:nth-child(2)')).to.equal('ausername')
   }
 
   async function logout() {
     // action: goto settings page
     await driver.get('http://localhost:3000/settings')
 
-    await driver.wait(until.elementLocated(By.css('button.btn-outline-danger')))
+    await waitFor('button.btn-outline-danger')
 
     // action: click logout button
-    const logoutButton = await driver.findElement(By.css('button.btn-outline-danger'))
-    await logoutButton.click()
+    await click('button.btn-outline-danger')
   }
 
   async function validateBlog() {
@@ -174,40 +157,32 @@ describe('registration page', function() {
     await driver.get('http://localhost:3000/')
 
     // validate post author
-    const postUsername = await driver.findElement(By.css('a.author'))
-    expect(await postUsername.getText()).to.equal('ausername')
+    expect(await getText('a.author')).to.equal('ausername')
 
     // validate post title
-    const postTitle = await driver.findElement(By.css('.article-preview h1'))
-    expect(await postTitle.getText()).to.equal('a title')
+    expect(await getText('.article-preview h1')).to.equal('a title')
 
     // validate post description
-    const postDescription = await driver.findElement(By.css('.article-preview h1 + p'))
-    expect(await postDescription.getText()).to.equal('something')
+    expect(await getText('.article-preview h1 + p')).to.equal('something')
 
     // validate post tags
-    const postTags = await driver.findElement(By.css('.article-preview ul'))
-    expect(await postTags.getText()).to.equal('abc')
+    expect(await getText('.article-preview ul')).to.equal('abc')
 
     // validate popular tags
-    const popularTags = await driver.findElement(By.css('.sidebar .tag-list'))
-    expect(await popularTags.getText()).to.equal('abc')
+    expect(await getText('.sidebar .tag-list')).to.equal('abc')
 
     // action: goto post page
-    await postTitle.click()
-    await driver.wait(until.elementLocated(By.css('.article-page')))
+    await click('.article-preview h1')
+    await waitFor('.article-page')
 
     // validate post title
-    const title = await driver.findElement(By.css('h1'))
-    expect(await title.getText()).to.equal('a title')
+    expect(await getText('h1')).to.equal('a title')
 
     // validate article description
-    const article = await driver.findElement(By.css('div.article-content p'))
-    expect(await article.getText()).to.equal('wonderful')
+    expect(await getText('div.article-content p')).to.equal('wonderful')
 
     // validate article tags
-    const tags = await driver.findElement(By.css('ul.tag-list'))
-    expect(await tags.getText()).to.equal('abc')
+    expect(await getText('ul.tag-list')).to.equal('abc')
 
     // ...
   }
