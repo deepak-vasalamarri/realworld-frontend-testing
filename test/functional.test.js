5d8920156b2749e4998baf8928d722f0aca49680
Gil Tayar <gil@tayar.org>
2018-03-14 11:06:57 +0200

    removed the second test

--- test/functional.test.js
+++ test/functional.test.js
@@ -34,31 +34,4 @@ describe('registration page', function() {
 
     expect(await errorMessages.getText()).to.include("email can't be blank")
   })
-
-  it('should not allow an invalid email', async () => {
-    // action: browse to registration page
-    await driver.get('http://localhost:3000/register')
-    await driver.wait(until.elementLocated(By.css('input[placeholder=Username]')))
-
-    // action: set username and password to something
-    const userNameField = await driver.findElement(By.css('input[placeholder=Username]'))
-    await userNameField.sendKeys('aUsername')
-
-    const passwordField = await driver.findElement(By.css('input[placeholder=Password]'))
-    await passwordField.sendKeys('aPassword')
-
-    // action: set email to invalid field
-    const emailField = await driver.findElement(By.css('input[placeholder=Email]'))
-    await emailField.sendKeys('abademail@foo')
-
-    // action: submit form
-    const submit = await driver.findElement(By.css('button[type=submit]'))
-    await submit.click()
-
-    // validate: there is a correct error message
-    await driver.wait(until.elementLocated(By.css('[class=error-messages]')))
-
-    const errorMessages = await driver.findElement(By.css('[class=error-messages]'))
-    expect(await errorMessages.getText()).to.include('email is invalid')
-  })
 })
